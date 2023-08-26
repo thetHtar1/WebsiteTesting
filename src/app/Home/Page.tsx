cab4a5903527d1b777496984150f73d68811de87
Thet Htar Wai <SmilaxCloud\thethtar.wai>
2023-08-26 17:10:49 +0630

    'update'

---
+++ src/app/Home/Page.tsx
@@ -0,0 +1,20 @@
+import Menu from "@/Components/menu/Menu"
+import HomeBodyComponent from '../../Components/HomeBody/HomeBody'
+import FooterCurve from "@/Components/FooterCurve"
+import Projects from "@/Components/Projects"
+import SlidingImages from "@/Components/SlidingImages"
+import Footer from "@/Components/footer/Footer"
+
+const HomePage=()=>{
+    return(
+        <div>
+            <Menu/>
+            <HomeBodyComponent/>
+            <Projects/>
+<SlidingImages/>
+<FooterCurve/>
+    <Footer/>
+        </div>
+    )
+}
+export default HomePage
---
+++ src/app/meet-the-team/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import  staffimg  from "../../../public/htetsu.jpg";
+import Image from "next/image";
+import './team.css'
+import Menu from '@/Components/menu/Menu';
+import FooterCurve from '@/Components/FooterCurve';
+import Footer from '@/Components/footer/Footer';
+import MeetTeamComponent from '@/Components/meetteamComponent'
+export default function MeetTheTeam() {
+  return (
+    <>
+    <Menu/>
+    <MeetTeamComponent/>
+
+
+    <FooterCurve/>
+<Footer/>
+    </>
+  )
+}
